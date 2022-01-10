ec88bc1a8be14c841f62758ba99dbff2b908dc53
charlesbrams <97448810+charlesbrams@users.noreply.github.com>
2022-01-10 13:47:36 +0100

    Add files via upload

--- vineup/vine_up/profile.php
+++ vineup/vine_up/profile.php
@@ -10,15 +10,15 @@
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js" integrity="sha512-894YE6QWD5I59HgZOGReFYm4dnWc1Qt5NtvYSaNcOP+u1T9qYdvdihz0PPSiiqn/+/3e7Jo4EaG7TubfWGUrMQ==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
 
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta2/css/all.min.css" integrity="sha512-YWzhKL2whUzgiheMoBFwW8CKV4qpHQAEuvilg9FAn5VJUDwKZZxkJNuGM4XkWuk94WCrrwslk8yWNGmY1EduTA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
-        <title> VINE UP </title>
+        <title> Profile </title>
     </head>
 
     <body>
         <?php include_once("inc/header/header.php") ?>
 
         
-
-
+        </br></br>Page profile </br>
+        <a href="inc/co_ins/deconnection.php"> déconnection </a>
         <footer>
 
         </footer>
