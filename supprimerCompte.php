237940a4562aa3a2be0c0b6981942ef9c62e8ba4
Simon Roffet <93921932+Sitronc@users.noreply.github.com>
2022-06-22 14:44:33 +0200

    Delete supprimerCompte.php

--- supprimerCompte.php
+++
@@ -1,8 +0,0 @@
-<?PHP
-//function getIdBySession(){
-    //session_id(); 
-//}
-function supprimerCompte(){
-    mysql_query("DELETE FROM utilisateurs WHERE id="sessions_id")}
-    echo 'Données supprimées'
-?>
