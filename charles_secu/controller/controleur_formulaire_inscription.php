8774e76db527925cd56f7e135b63b08c5dd06abb
Simon Roffet <93921932+Sitronc@users.noreply.github.com>
2022-06-22 14:42:03 +0200

    Delete charles_secu directory

--- charles_secu/controller/controleur_formulaire_inscription.php
+++
@@ -1,4 +0,0 @@
-<?php
-    //vérification qu'il ne cree pas plusieurs compte d'affiler, que ce ne  soit pas un robot
-    include_once ("formulaire_inscription.php") ;
-?>
--- charles_secu/controller/supprimerProfil.php
+++
@@ -1,7 +0,0 @@
-<?php 
-session_start();
-$IdUtilisateur = session_id();
-include_once('../model/model.php');
-supprimer_compte($IdUtilisateur);
-header('Location:../view/connexion.php');
-?>
