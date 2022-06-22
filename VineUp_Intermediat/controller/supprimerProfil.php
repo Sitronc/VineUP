672890d4e7ae07fd4cd5199416512b1cc510a3fb
Simon Roffet <93921932+Sitronc@users.noreply.github.com>
2022-06-22 14:41:48 +0200

    Delete VineUp_Intermediat directory

--- VineUp_Intermediat/controller/supprimerProfil.php
+++
@@ -1,9 +0,0 @@
-<?php 
-session_start();
-session_destroy();
-$IdUtilisateur = session_id();
-include_once('../model/model.php');
-supprimer_compte($IdUtilisateur);
-
-header('Location:../view/connexion.php');
-?>
